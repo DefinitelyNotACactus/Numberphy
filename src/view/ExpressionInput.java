38ed74b145e8c9c13e0e7e5bdebffa84b70ad868
Victor Koehler <koehlervictor@cc.ci.ufpb.br>
2019-04-01 19:39:00 -0300

    IrregularFunction + Refactor of IntervalFunction (#20)

--- src/view/ExpressionInput.java
+++ src/view/ExpressionInput.java
@@ -39,6 +39,7 @@ import data.MethodsEnum;
 import data.Constants;
 import data.Hermite;
 import data.Spline;
+import data.IntervalFunctionComposition;
 
 import edu.hws.jcm.awt.Controller;
 import edu.hws.jcm.awt.InputObject;
@@ -839,57 +840,6 @@ public class ExpressionInput extends JPanel implements InputObject, Value {
     public void processTextEvent(TextEvent evt) {
         if (onTextChange != null) {
             onTextChange.compute();
-        }
-    }
-
-    
-    public class IntervalFunctionComposition {
-        
-        private class Tuple<T,U> {
-            private final T t;
-            private final U u;
-
-            public Tuple(T key, U value) {
-                this.t = key;
-                this.u = value;
-            }
-
-            /**
-             * @return the t
-             */
-            public T getKey() {
-                return t;
-            }
-
-            /**
-             * @return the u
-             */
-            public U getValue() {
-                return u;
-            }
-            
-        }
-        
-        private final Stack<Tuple<Double, Function>> stack;
-        
-        public IntervalFunctionComposition(Function initial)
-        {
-            stack = new Stack<>();
-            stack.add(new Tuple(Double.NEGATIVE_INFINITY, initial));
-        }
-        
-        public void stackFunction(Function func, double xi) {
-            stack.add(new Tuple(xi, func));
-        }
-        
-        public double eval(double x) {
-            for (int i = stack.size() - 1; i >= 0; i--)
-            {
-                Tuple<Double, Function> t = stack.get(i);
-                if (x > t.getKey())
-                    return t.getValue().getVal(new double[] { x });
-            }
-            return Double.NaN;
         }
     }
 
