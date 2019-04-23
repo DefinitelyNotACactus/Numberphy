19681579d0123c7561846c02402b4484e0aeef70
DefinitelyNotACactus <galvao2010@icloud.com>
2019-04-23 11:14:18 -0300

    Add abstractGauss

---
+++ src/data/AbstractGauss.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package data;
+
+import edu.hws.jcm.data.SimpleFunction;
+import view.ExpressionInput;
+import view.MethodImplementation;
+
+/**
+ *
+ * @author nullPointerException
+ */
+public abstract class AbstractGauss implements MethodImplementation {
+    private final ExpressionInput input;
+    
+    public AbstractGauss(ExpressionInput input, String function, double a, double b, int numPoints) {
+        this.input = input;
+    }
+    
+    protected ExpressionInput getInput() {
+        return input;
+    }
+    
+    public abstract double getArea();
+    
+    public abstract Iteration[] getIterations();
+    
+    public abstract SimpleFunction getComputedFunction();
+    
+    
+}
